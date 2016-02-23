d77a2878b5fb7f17b046c8643cf1f39a86f9253d
James Gray <jamesgray@ischool.berkeley.edu>
2016-02-22 22:01:39 -0600

    commit HW7
    posting HW7

---
+++ Week7/google4up.py
@@ -0,0 +1,30 @@
+import object_storage
+import os
+import time
+
+DATAFILE = os.path.expanduser("~/onedrive/github/Berkeley_W251/googlebooks.csv")
+googlefilesize = 1596  
+
+# connect to storage account
+sl_storage = object_storage.get_client('SLOS856755-2:SL856755',
+                                       'cdc24208f9bb1aff86c22d9474b79b5333c654d96503ae9761979d5eb9650ae8', 
+                                       datacenter='dal05')
+
+try:
+    sl_storage['week7']['googlebooks4.csv'].create()
+
+except ResponseError:
+    print ResponseError
+    
+try:
+    uploadstart = time.time()
+    fileUpload = sl_storage['week7']['googlebooks4.csv'].load_from_filename(DATAFILE)
+except object_storage.errors.ResponseError:
+    print ResponseError
+
+uploadcomplete = time.time()
+
+uploadtime = uploadcomplete - uploadstart
+print "Total upload time " + str(uploadtime) + " seconds"
+uploadspeed = googlefilesize/uploadtime
+print "Upload speed = " + str(uploadspeed) + " MB/sec"
